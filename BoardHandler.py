2c12c3f47e3f22e5d6899d5aea26601c2b9dbd53
Intergalactic Emperor Vineeth <vineeth.h91@gmail.com>
2016-02-25 22:01:01 -0800

    Added board handler mapping for led matrix pins -> raspberry pi pins + RPi pinout for reference

--- BoardHandler.py
+++ BoardHandler.py
@@ -5,6 +5,28 @@
 #  *Diagnostics 
 #  *Send/Get image
 
+# For the below reference RP2_Pingout.png
+#
+# Panel Pin     RPi 	Notes
+# R0	1	7 	GPIO4
+# G0	2	11 	GPIO17
+# B0	3	13	GPIO27
+# GND	4	9	GND
+# R1	5	15	GPIO22
+# G1	6	19	GPIO10
+# B1	7	21	GPIO09
+# GND	8	25	GND
+# A	9	23	GPIO11
+# B	10	29	GPIO5
+# C	11	31	GPIO6
+# D	12	33	GPIO13
+# CLK	13	35	GPIO19
+# STB	14	37	GPIO26
+# OE	15	40	GPIO21
+# GND	16	39	GND
+
+
+
 import RPi.GPIO as GPIO
 import time
 
