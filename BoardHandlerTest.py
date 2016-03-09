8bee91be3914528b46018cec67b1486898641483
Intergalactic Emperor Vineeth <vineeth.h91@gmail.com>
2016-03-09 00:02:52 -0800

    Added basic letter mapping that can be scaled and printed to board

--- BoardHandlerTest.py
+++ BoardHandlerTest.py
@@ -3,14 +3,19 @@
 from BoardHandler import BoardHandler
 from ImageBufferCreator import ImageBufferCreator 
 import numpy as npy 
+import BasicMappings as MAP 
 
 class BoardHandlerTest:
  
+    board = None 
+    list = [MAP.A, MAP.B, MAP.C, MAP.D]
+
     # Test User initiatied board initialization
     def initializeBoardTest(self):
         print "Testing BoardHandler initiazilation"
-        board = BoardHandler()
-        board.initializeBoard()
+        self.board = BoardHandler()
+        self.board.initializeBoard()
+        self.board.initializePins()
 
     def getBufferWithFill(self, fill):
         # initialize entire 32x32x3 matrix to all 1s for a white background 
@@ -20,11 +25,27 @@ class BoardHandlerTest:
         # initialize entire 32x32x3 matrix to r/g/b background 
         return [[[r, g, b] for j in range(32)] for i in range(32)]
 
+    def printLetters(self):
+        print "Printing Letters of various sizes to Board"
+        index = 0
+        while True:
+          for i in range(1,5):
+            # kronecker product to scale the matrix for the various size(scale factor of 8x)
+            newA = npy.kron(self.list[index], npy.ones((i,i)))
+            buffer = self.getBufferWithFill(0)
+            for k in range(8*i):
+              for j in range(8*i):
+                if(newA[k][j] != 0):
+                  buffer[k][j] = [0,1,0] 
+            for z in range(40):
+              self.board.printBufferToBoard(buffer)
+          index += 1
+          if(index >= len(self.list)):
+            index = 0
+
     def testDifferentDiagnostics(self):
+
         print "Printing Buffer to Board"
-        board = BoardHandler()
-        board.initializeBoard()
-        board.initializePins()
 
         buffer = self.getBufferWithFill(0)
         #buffer = self.getBufferWithRGBBackground(0,1,0)
@@ -42,7 +63,7 @@ class BoardHandlerTest:
 
         count = 2000
         while True:
-            board.printBufferToBoard(buffer)
+            self.board.printBufferToBoard(buffer)
             buffer = npy.roll(buffer, -1, axis = 1)
 
     def drawShapes(self):
@@ -70,6 +91,9 @@ class BoardHandlerTest:
         while True:
             board.printBufferToBoard(buffer)
 
+
 boardHandlerTest = BoardHandlerTest() 
+boardHandlerTest.initializeBoardTest()
+boardHandlerTest.printLetters()
 boardHandlerTest.testDifferentDiagnostics()
 boardHandlerTest.drawShapes()
